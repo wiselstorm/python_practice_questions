5cf6a0b2e273026afdc68733ee28baa2d8b8ee39
Timotej Nidorfer <58996951+wiselstorm@users.noreply.github.com>
2022-10-07 15:30:24 +0200

    Exercise 7: Return the count of a given substring from a string

---
+++ find_substring.py
@@ -0,0 +1,5 @@
+str_x = input("Enter desired sentence\n")
+query_string=input("What do you wanna search?\n")
+counter=str_x.count(query_string)
+
+print("Word ",query_string, "was found: "," ",counter,"Times")
